9c9a6a033ebdf8fc47af50f951f7888eef4f9d4e
Ragavendaran Puliyadi <ragav@op.in.eu.org>
2025-04-30 11:29:07 +0530

    src: sample dev usage

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { GeometryDash } from "./lib/GeometryDash/Source";
+import { FilePersistence } from "./lib/State";
+
+const gd = new GeometryDash(new FilePersistence("data/state.json"));
+
+let res;
+
+// res = await gd.getGJGauntlets()
+res = await gd.getGJTopArtists()
+
+console.log(res)
